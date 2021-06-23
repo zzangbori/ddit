7fe90b0eb54a4cedd8ff7d9e04bde87da9002e13
tablet7823 <86089980+tablet7823@users.noreply.github.com>
2021-06-23 10:33:41 +0900

    Create Calculator.java

---
+++ JAVA_homework/ObjectOrientedProgramming/Calculator/Calculator.java
@@ -0,0 +1,23 @@
+package z_homework;
+
+public class Calculator {
+	double add(double a, double b) {
+		return a + b;
+	}
+
+	double subtract(double a, double b) {
+		return a - b;
+	}
+
+	double multiply(double a, double b) {
+		return a * b;
+	}
+
+	double divide(double a, double b) {
+		return a / b;
+	}
+
+	double remainder(double a, double b) {
+		return a % b;
+	}
+}
