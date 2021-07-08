1cf378c1ffa643f5ab575625de6dadb17144d4c6
tablet7823 <86089980+tablet7823@users.noreply.github.com>
2021-07-08 19:06:58 +0900

    Update Ddit.java

--- JAVA_homework/JAVABasicOOPTest/Ddit.java
+++ JAVA_homework/JAVABasicOOPTest/Ddit.java
@@ -1,1 +1,16 @@
-
+public class Ddit {
+	private ClassRoom[] classRooms = new ClassRoom[8];
+	private int totalClassRoom;
+	
+	public void addClassRoom(ClassRoom classRoom) {
+	}
+	
+	public ClassRoom getClassRoom(int index) {
+	}
+	
+	public int getTotalClassRoom() {
+	}
+	
+	public void showClassRoomList() {
+	}
+}
