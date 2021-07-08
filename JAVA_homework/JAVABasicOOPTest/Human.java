aeea874a92b362c5fdd829162908409c8bd4e08e
tablet7823 <86089980+tablet7823@users.noreply.github.com>
2021-07-08 19:07:38 +0900

    Update Human.java

--- JAVA_homework/JAVABasicOOPTest/Human.java
+++ JAVA_homework/JAVABasicOOPTest/Human.java
@@ -1,1 +1,17 @@
-
+public class Human {
+	private String name, gender, job;
+	private int age;
+	
+	public Human(String name, String gender, String job, int age) {
+	}
+	
+	public String getName() {
+	}
+	public String getGender() {
+	}
+	public int getAge() {
+	}
+	public String getJob() {
+	}
+	
+}
