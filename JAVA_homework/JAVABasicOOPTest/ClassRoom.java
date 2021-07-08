99cf299741bdbc917de2656075b0835b0285f806
tablet7823 <86089980+tablet7823@users.noreply.github.com>
2021-07-08 19:06:38 +0900

    Update ClassRoom.java

--- JAVA_homework/JAVABasicOOPTest/ClassRoom.java
+++ JAVA_homework/JAVABasicOOPTest/ClassRoom.java
@@ -1,1 +1,29 @@
-
+public class ClassRoom {
+	private int classNumber;
+	private Student[] students = new Student[5];
+	private Teacher teacher;
+	private int totalStudent;
+	
+	public ClassRoom(int classNumber, Teacher teacher) {
+	}
+	
+	public int getClassNumber() {
+	}
+	
+	public boolean addStudent(Student student) {
+		try {
+		}catch (ArrayIndexOutOfBoundsException e) {
+		}
+	}
+	
+	public void showClassInfo() {
+	}
+	
+	public void showStudentInfo(Human human, int index) {
+		try {
+		}catch (NotTeacherException e) {
+		}catch (NotConfirmException e) {
+		}catch (ArrayIndexOutOfBoundsException e) {
+		}
+	}
+}
